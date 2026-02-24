9d53d339729cdcdd88e8f88e341e091f6518dff2
Anna8230094 <annamegalou3@gmail.com>
2026-02-24 23:28:18 +0200

    initialize oriject

---
+++ ubookit/src/main/java/com/tracking/ubookit/UbookitApplication.java
@@ -0,0 +1,13 @@
+package com.tracking.ubookit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbookitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbookitApplication.class, args);
+	}
+
+}
---
+++ ubookit/src/test/java/com/tracking/ubookit/UbookitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracking.ubookit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbookitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
